fcba96d6df4e9a9a3301cb27ee764f129c69f2f4
Rashi <jhar7605@gmail.com>
2025-06-29 19:48:16 -0500

    More content added

--- data_import.py
+++ data_import.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 
 print("Current working directory:", os.getcwd())
@@ -16,9 +18,25 @@ print(df.describe())
 print("Number of non-null values in each column:")
 emp=df.notnull().sum()
 print(f"Sum of non null valules {emp}")
-#print(df.notnull().sum())
-print(df['CustomerID'].nunique())  # prints the number of unique values in the 'CustomerID' column
-print(df['Gender'].nunique())
-print(df['Age'].nunique())
-print(df['Annual Income (k$)'].nunique())
-print(df.columns)
+print(df.isnull().sum())
+print("Number of null values in each column:")
+null_values = df.isnull().sum()
+print(null_values)
+dup=df.duplicated().sum()
+print(f"Number of duplicate rows: {dup}")
+sns.set_style(style= "whitegrid")
+sns.countplot(data=df, x='Gender',edgecolor='black', palette='pastel')
+plt.title('Customer Gender Distribution')
+plt.xlabel('Gender')
+plt.ylabel('Count') #bar chart for Gender distribution
+plt.show()
+plt.hist(df['Age'],bins=9, edgecolor= 'blue',color='orange')
+plt.title('Customer Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Count')  # histogram for Age distribution
+plt.show()
+sns.scatterplot(data=df, x='Annual Income (k$)',y='Spending Score (1-100)',edgecolor='yellow', palette='orange')
+plt.title('Annual Income vs Spending Score')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')  # scatter plot for Annual Income vs Spending Score
+plt.show()
